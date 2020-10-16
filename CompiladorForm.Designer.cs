5c74e06bed1c0edb28656f679e3a73dbbab002c8
Fabio Irokawa <fabio.irokawa1@hotmail.com>
2020-10-16 01:36:45 -0300

    pequenas melhorias

--- CompiladorForm.Designer.cs
+++ CompiladorForm.Designer.cs
@@ -43,13 +43,14 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(820, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1093, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -60,39 +61,41 @@
             this.salvarToolStripMenuItem,
             this.compilarToolStripMenuItem});
             this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
             this.menuToolStripMenuItem.Text = "Menu";
             // 
             // abrirToolStripMenuItem
             // 
             this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
-            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
             this.abrirToolStripMenuItem.Text = "Abrir";
             this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
             // 
             // salvarToolStripMenuItem
             // 
             this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
-            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
             this.salvarToolStripMenuItem.Text = "Salvar";
             this.salvarToolStripMenuItem.Click += new System.EventHandler(this.salvarToolStripMenuItem_Click);
             // 
             // compilarToolStripMenuItem
             // 
             this.compilarToolStripMenuItem.Name = "compilarToolStripMenuItem";
-            this.compilarToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.compilarToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
             this.compilarToolStripMenuItem.Text = "Compilar";
             this.compilarToolStripMenuItem.Click += new System.EventHandler(this.compilarToolStripMenuItem_Click);
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.richTextBox1.Location = new System.Drawing.Point(52, 21);
+            this.richTextBox1.Location = new System.Drawing.Point(100, 29);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(1, 0, 0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(756, 298);
+            this.richTextBox1.Size = new System.Drawing.Size(977, 366);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             this.richTextBox1.WordWrap = false;
@@ -101,50 +104,56 @@
             this.richTextBox1.Click += new System.EventHandler(this.richTextBox1_Click);
             this.richTextBox1.FontChanged += new System.EventHandler(this.richTextBox1_FontChanged);
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RichTextBox1_KeyDown);
             // 
             // LineNumberTextBox
             // 
             this.LineNumberTextBox.BackColor = System.Drawing.Color.White;
-            this.LineNumberTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LineNumberTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.LineNumberTextBox.Cursor = System.Windows.Forms.Cursors.PanNE;
             this.LineNumberTextBox.Enabled = false;
+            this.LineNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LineNumberTextBox.ForeColor = System.Drawing.Color.Black;
-            this.LineNumberTextBox.Location = new System.Drawing.Point(11, 21);
-            this.LineNumberTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.LineNumberTextBox.Location = new System.Drawing.Point(16, 29);
+            this.LineNumberTextBox.Margin = new System.Windows.Forms.Padding(0);
             this.LineNumberTextBox.Name = "LineNumberTextBox";
             this.LineNumberTextBox.ReadOnly = true;
             this.LineNumberTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.LineNumberTextBox.Size = new System.Drawing.Size(36, 298);
-            this.LineNumberTextBox.TabIndex = 2;
+            this.LineNumberTextBox.Size = new System.Drawing.Size(71, 366);
+            this.LineNumberTextBox.TabIndex = 1;
             this.LineNumberTextBox.Text = "";
             this.LineNumberTextBox.TextChanged += new System.EventHandler(this.LineNumberTextBox_TextChanged);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(11, 343);
+            this.richTextBox2.Location = new System.Drawing.Point(15, 422);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.ReadOnly = true;
-            this.richTextBox2.Size = new System.Drawing.Size(731, 95);
+            this.richTextBox2.Size = new System.Drawing.Size(973, 116);
             this.richTextBox2.TabIndex = 3;
             this.richTextBox2.Text = "";
             // 
             // textBox1
             // 
             this.textBox1.AllowDrop = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(12, 324);
+            this.textBox1.Location = new System.Drawing.Point(16, 399);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.Size = new System.Drawing.Size(133, 15);
             this.textBox1.TabIndex = 4;
             this.textBox1.Text = "Console:";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(748, 357);
+            this.button1.Location = new System.Drawing.Point(997, 439);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(60, 60);
+            this.button1.Size = new System.Drawing.Size(80, 74);
             this.button1.TabIndex = 5;
             this.button1.Text = "Limpar Console";
             this.button1.UseVisualStyleBackColor = true;
@@ -152,9 +161,10 @@
             // 
             // CompiladorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1093, 554);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox2);
@@ -162,6 +172,7 @@
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CompiladorForm";
             this.Text = "Compilador";
             this.Load += new System.EventHandler(this.CompiladorForm_Load);
@@ -181,10 +192,12 @@
         private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem compilarToolStripMenuItem;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox LineNumberTextBox;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox LineNumberTextBox;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
     }
+
+
 }
 
--- CompiladorForm.cs
+++ CompiladorForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using static Compilador.Constantes;
@@ -13,11 +14,13 @@ namespace Compilador
         Sintatico sintatico = new Sintatico();
         private int lastIndex = 0;
         private int lastLength = 0;
-
+        public List<int> KeyCodes = new List<int>() { 17, 86 }; //ctrl + v
+        public int storeLastLine = -1;
         public CompiladorForm()
         {
             InitializeComponent();
             AddLineNumbers();
+            //richTextBox1.BindScroll(LineNumberTextBox);
         }
 
         public int getWidth()
@@ -38,14 +41,14 @@ namespace Compilador
             {
                 w = 50 + (int)richTextBox1.Font.Size;
             }
-
+         
             return w;
         }
 
         public void AddLineNumbers()
         {
             // create & set Point pt to (0,0)    
-            Point pt = new Point(0, 0);
+            Point pt = new Point(0, 3);
             // get First Index & First Line from richTextBox1    
             int First_Index = richTextBox1.GetCharIndexFromPosition(pt);
             int First_Line = richTextBox1.GetLineFromCharIndex(First_Index);
@@ -56,15 +59,17 @@ namespace Compilador
             int Last_Index = richTextBox1.GetCharIndexFromPosition(pt);
             int Last_Line = richTextBox1.GetLineFromCharIndex(Last_Index);
             // set Center alignment to LineNumberTextBox    
-            LineNumberTextBox.SelectionAlignment = HorizontalAlignment.Center;
+            LineNumberTextBox.SelectionAlignment = HorizontalAlignment.Right;
             // set LineNumberTextBox text to null & width to getWidth() function value    
             LineNumberTextBox.Text = "";
-            LineNumberTextBox.Width = getWidth();
-            // now add each line number to LineNumberTextBox upto last line    
-            for (int i = First_Line; i <= Last_Line + 1; i++)
-            {
-                LineNumberTextBox.Text += i + 1 + "\n";
-            }
+            //LineNumberTextBox.Width = getWidth();
+            // now add each line number to LineNumberTextBox upto last line   
+
+                for (int i = First_Line; i < Last_Line + 1; i++)
+                {
+                    LineNumberTextBox.Text += i + 1 + "\n";
+                }
+            
             LineNumberTextBox.Invalidate();
         }
 
@@ -88,7 +93,7 @@ namespace Compilador
         {
             LineNumberTextBox.Text = "";
             AddLineNumbers();
-            //LineNumberTextBox.Invalidate();
+            LineNumberTextBox.Invalidate();
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -97,13 +102,36 @@ namespace Compilador
             {
                 AddLineNumbers();
             }
+            richTextBox1.Font = new Font("Microsoft Sans Serif", 9);
         }
-
+        
         private void richTextBox1_FontChanged(object sender, EventArgs e)
         {
             LineNumberTextBox.Font = richTextBox1.Font;
             richTextBox1.Select();
             AddLineNumbers();
+        }
+
+        private void RichTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (KeyCodes.Contains(e.KeyValue) && (e.KeyCode == Keys.V && e.Control))
+            {
+                richTextBox1.SelectedText = "";
+                //LineNumberTextBox.Text = "";
+                
+                //richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+                AddLineNumbers();
+            }
+
+        }
+
+        public void MarkSingleLine()
+        {
+            int firstCharOfLineIndex = richTextBox1.GetFirstCharIndexOfCurrentLine();
+            int currentLine = richTextBox1.GetLineFromCharIndex(firstCharOfLineIndex);
+            richTextBox1.SelectionStart = currentLine;
+            richTextBox1.SelectionBackColor = Color.Aqua;
         }
 
         private void LineNumberTextBox_MouseDown(object sender, MouseEventArgs e)
@@ -243,5 +271,11 @@ namespace Compilador
         {
             richTextBox2.Text = "";
         }
+
     }
+
+
+
+
+
 }
